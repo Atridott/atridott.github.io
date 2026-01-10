030db5273af061e6ba03a13f71730bcf1604a718
Atridott <atridott@gmail.com>
2026-01-11 00:38:42 +0900

    setup GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite"
 // https://astro.build/config
 export default defineConfig({
   site: "https://atridott.github.io",
+  output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
