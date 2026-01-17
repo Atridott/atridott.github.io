46cbfce0cfd0c2c9bb3703f50ff3b69e43d362ae
Atridott <atridott@gmail.com>
2026-01-18 00:55:33 +0900

    address animation and accessibility

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss"
+
+export default {
+	content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+
+			}
+		}
+	}
+} satisfies Config
