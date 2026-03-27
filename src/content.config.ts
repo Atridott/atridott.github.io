581c7fb885602c42ab7b98fbf53c278214f771d1
atridott <atridott@gmail.com>
2026-03-27 14:48:48 +0900

    chore: define temperature(`tone`)

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,7 @@ import { z } from "astro/zod";
 const notes = defineCollection({
 	loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/notes" }),
 	schema: z.object({
+		tone: z.enum(["warm", "cool"]),
 		title: z.string(),
 		emoji: z.emoji(),
 	})
