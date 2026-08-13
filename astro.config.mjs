4804b30aac2338e419bb30b3828992a21fd7894f
atridott <atridott@gmail.com>
2026-08-13 18:49:56 +0900

    disabled devToolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://atridott.github.io",
+   site: "https://atridott.github.io",
+   devToolbar: { enabled: false },
 });
